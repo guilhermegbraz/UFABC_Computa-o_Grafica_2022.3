f0e3d146034b01b18a91562bf963bafe35edd0ca
Guilherme Gonçalves Braz <84741834+guilhermegbraz@users.noreply.github.com>
2022-10-04 20:21:23 -0300

    Atividade 2 finalizada
    Fim da att 2 - Revisão C++ e GUI

---
+++ Semana 02/Atividade 2/Atividade-02/collor.cpp
@@ -0,0 +1,8 @@
+#include "collor.h"
+
+Collor::Collor()
+{}
+
+void Collor::selectedCollor(int d) {
+    collorId = d;
+}
---
+++ Semana 02/Atividade 2/Atividade-02/collor.h
@@ -0,0 +1,15 @@
+#ifndef COLLOR_H
+#define COLLOR_H
+
+
+class Collor
+{
+public:
+    Collor();
+    int collorId{0};
+
+public slots:
+    void selectedCollor(int);
+};
+
+#endif // COLLOR_H
